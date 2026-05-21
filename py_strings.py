138cc92311b4354c701451cc38bdc88c78a3f9ba
Ayushi Prajapati <ayushi016750@gmail.com>
2026-05-21 19:12:32 +0530

    Started python journey

---
+++ py_strings.py
@@ -0,0 +1,18 @@
+a = "hello! how are you doing?"
+print(a[7:10])
+print(a[:])
+
+x = "Rahul"
+y = 20
+
+print(f"{x} is {y} years old")
+print(x,"is",y,"years old")
+
+#Taking input
+
+#using only 'input' will take input as string
+age = input("What is your age? ")
+print(f"Your age is {age}")
+
+age2 = int(input("What is your age? "))
+print(f"Your age is {age2+1}")
