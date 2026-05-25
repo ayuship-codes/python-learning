e6e543250c055170af7254331fb57f50a03594f3
Ayushi Prajapati <ayushi016750@gmail.com>
2026-05-25 23:42:07 +0530

    Implemented list methods in Python

---
+++ list.py
@@ -0,0 +1,16 @@
+l1 = [12,4,56,23,9,5,2,4,22,0]
+
+# list methods
+l1.append(99)
+print(l1)
+l1.remove(5)  # removes element 
+print(l1)
+l1.pop(3)   # removes element by index
+print(l1)
+l1.pop()   # removes last element
+print(l1)
+l1.extend([3,2,1])
+print(l1)
+print(l1.index(0))
+
+print(l1.count(4))
